52a80bbab250e0fc660ae91f4347bad9b2f2c70d
RuhamaMalik <malikruhama7@gmail.com>
2023-11-02 18:07:43 +0500

    events and slider

---
+++ 2-11-23/app.js
@@ -0,0 +1,61 @@
+////////////////////Setting field values
+
+// function fillCity() {
+//   var cityName;
+//   var zipCode= document.getElementById("zip").value;
+//   switch (zipCode) {
+//     case "1111":
+//       cityName = "Karachi";
+//       break;
+//     case "2222":
+//       cityName = "Peshawar";
+//       break;
+//     case "3333":
+//       cityName = "Islamabad";
+//       default:
+//       alert('Invalid zip');
+//   }
+//   if(cityName === undefined){
+//     document.getElementById("city").value = '';
+//   }else{
+//     document.getElementById("city").value = cityName;
+//   }
+
+// }
+
+/////////////////// Reading and setting paragraph text
+
+// function expandPara() {
+//   var expandedParagraph = " Lorem ipsum dolor sit amet consectetur adipisicing elit.  Praesentium molestias magnam id eos obcaecati, aperiam ipsa nobis necessitatibus incidunt, itaque quis amet fugiat. Nulla recusandae sequi veritatis minus repudiandae aut.";
+//   document.getElementById("para").innerText = expandedParagraph;
+// }
+
+// function displayList() {
+//   var listToPlace =
+//     "<ol><li>First Item</li><li>Second Item</li><li>Third Item</li></ol>";
+//   document.getElementById("itemList").innerHTML = listToPlace;
+// }
+
+// function readParaText() {
+//   // var readPara = document.getElementById("content").innerHTML;
+//   var readPara = document.getElementById("content").innerText;
+//   console.log(readPara);
+// }
+
+/////////////// Manipulating images and text
+
+// function makeInvisible() {
+//   document.getElementById("ugly").className = "hidden";
+// }
+
+// function addClass() {
+//   document.getElementById("p1").className += " newClass";
+// }
+
+function makeBig() {
+  document.getElementById("btn").className = " big";
+}
+function makeNormal() {
+  document.getElementById("btn").className = " normal";
+}
+
